17889154254a10e75cecca1826403ca5c7be6015
AustralCoder <jnicolaskelly.2@gmail.com>
2026-04-16 19:25:30 -0300

    feat: check_enviroment function ready

--- main_scraper.py
+++ main_scraper.py
@@ -9,7 +9,19 @@ from pages.linkedin.login_page import LinkedInLogInPage
 from pages.linkedin.search_page import LinkedInJobSearchPage
 
 def check_enviroment():
-    #TODO
+    print("validating env file")
+
+    load_dotenv()
+
+    if not os.getenv(LINKEDIN_EMAIL) or not os.getenv(LINKEDIN_PASSWORD):
+        print("Fatal error missing credentials")
+        sys.exit(1)
+
+    if not os.path.exists(config/settings.json):
+        print("Fatal error missing .env file")
+        sys.exit(1)
+
+    print("All good.")
     pass
 
 def setup_driver():
