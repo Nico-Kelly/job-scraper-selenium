182b86e40613ffd9209669da382d22437fc66b26
AustralCoder <jnicolaskelly.2@gmail.com>
2026-04-20 21:59:19 -0300

    fix: minor fixes

--- main_scraper.py
+++ main_scraper.py
@@ -9,7 +9,7 @@ from pages.linkedin.login_page import LinkedInLogInPage
 from pages.linkedin.search_page import LinkedInJobSearchPage
 
 def check_enviroment():
-    print("validating env file")
+    print("validating json file")
 
     load_dotenv()
 
@@ -18,7 +18,7 @@ def check_enviroment():
         sys.exit(1)
 
     if not os.path.exists('config/settings.json'):
-        print("Fatal error missing .env file")
+        print("Fatal error missing .json file")
         sys.exit(1)
 
     print("All good.")
@@ -27,7 +27,7 @@ def setup_driver():
     options = Options()
     options.add_argument("--start-maximized")
     options.add_argument("--disable-notifications")
-    options.add_argument("--headless")
+    #options.add_argument("--headless")
 
     return webdriver.Chrome(options=options)
 
