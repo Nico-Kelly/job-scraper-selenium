36c952e8f458817252779a1d971c3b7fb096a2d4
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-04-22 22:13:13 -0300

    feat: created Google Sheets service skeleton and defined class interface

---
+++ services/google_sheets.py
@@ -0,0 +1,14 @@
+class GoogleSheetService:
+
+    def __init__(self, spreadsheet_id: str):
+        self.spreadsheet_id = spreadsheet_id
+        #TODO
+        print("Google Sheets Service initialized")
+
+    def authenticate(self):
+        #TODO
+        pass
+
+    def append_jobs(self, jobs_data: list):
+        #TODO
+        pass
