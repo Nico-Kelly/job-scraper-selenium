0a66168fff90230337cc1b15f82b1eed54eb5954
AustralCoder <jnicolaskelly.2@gmail.com>
2026-04-29 23:30:25 -0300

    refactor(models): implement __post_init__ in JobPosting to auto-sanitize data on instantiation

--- utils/models.py
+++ utils/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from utils.data_cleaner import DataCleaner
 
 @dataclass()
 class JobPosting:
@@ -8,3 +9,13 @@ class JobPosting:
     url: str
 
 
+    def __post_init__(self):
+
+        """
+        right after the dataclass assigns the variable values this applies the static methods from DataCleaner before the object
+        goes to the Google Sheets service
+        :return:
+        """
+        self.title = DataCleaner.clean_text(self.title)
+        self.company = DataCleaner.extract_company_name(self.company)
+        self.location = DataCleaner.clean_text(self.location)
