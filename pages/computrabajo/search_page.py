4c20ef319178d591a2949b9dc7d19cf58eccad54
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-05-16 22:33:15 -0300

    feat(pages): add structural skeleton for Computrabajo

---
+++ pages/computrabajo/search_page.py
@@ -0,0 +1,24 @@
+from pages.base_page import BasePage
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+class ComputrabajoSearchPage(BasePage):
+    
+    # LOCATORS
+    # TODO
+    def __init__(self, browser):
+        super().__init__(browser)
+        # TODO: 
+        
+    def load(self):
+        # TODO: 
+        pass
+
+    def search(self):
+        # TODO: 
+        pass
+        
+    def extract_job_cards(self):
+        # TODO: 
+        return []
