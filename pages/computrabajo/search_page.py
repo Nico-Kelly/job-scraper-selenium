d726fa3d0c8d5a90e8b482dd222851fd394ae010
AustralCoder <jnicolaskelly.2@gmail.com>
2026-05-19 22:41:15 -0300

    feat(computrabajo): implement init and load methods reading from settings.json

--- pages/computrabajo/search_page.py
+++ pages/computrabajo/search_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from utils.logger import get_logger
+import json
 
 logger = get_logger(__name__)
 
@@ -9,11 +10,18 @@ class ComputrabajoSearchPage(BasePage):
     # TODO
     def __init__(self, browser):
         super().__init__(browser)
-        # TODO: 
-        
+        with open('config/settings.json', 'r', encoding='utf-8') as file:
+            self.config = json.load(file)
+
+        self.url = self.config['urls']['computabrajo']
+        self.keyword = self.config['search_params']['job_title']
+        self.location = self.config['search_params']['location']
+        self.timeout = self.config['timeouts']['explicit_wait']
+
     def load(self):
-        # TODO: 
-        pass
+        logger.info(f"Loading Computrabajo {self.url}")
+        self.browser.get(self.url)
+
 
     def search(self):
         # TODO: 
