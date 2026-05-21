0e9cc2d42b53c85dbaaeec918784fcc0218fa8a8
AustralCoder <jnicolaskelly.2@gmail.com>
2026-05-21 19:40:37 -0300

    feat(utils): resolve #TODO to clean artifacts and newlines from company names

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -15,8 +15,10 @@ class DataCleaner:
 
     @staticmethod
     def extract_company_name(raw_company_text: str) -> str:
-        """
-        This method should remove any unexpected word.
-        """
-        #TODO
+        if not raw_company_text:
+            return ""
+
+        name = raw_company_text.split('·')[0]
+        name = name.split('\n')[0]
+
         return DataCleaner.clean_text(raw_company_text)
